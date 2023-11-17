59a795dbf0f90eef86e4270a04e42a66dfebc012
SteeveParis <117830853+SteeveParis@users.noreply.github.com>
2023-11-17 11:59:32 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,7 @@
-
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("********************************************");
+    System.out.println("Hello le monde");
+    System.out.println("********************************************");
+  }
+}
